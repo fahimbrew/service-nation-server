850dd692ebd4b465b72010d5e1c6109eecbc49c3
fahimbrew <iftiaqhossain@gmail.com>
2025-04-07 11:02:38 +0600

    single data entry has been completed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.get("/",(req,res)=>{
     res.send("Hello from the server side")
 })
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.p7zpg.mongodb.net/?appName=Cluster1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,13 @@ async function run() {
      
     app.get("/services",async(req,res)=>{
         const result = await serviceCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get("/service/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
         res.send(result);
     })
 
