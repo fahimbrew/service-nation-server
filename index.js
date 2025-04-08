fe19373c27ccf85a8502b9a0256365f94303a98c
fahimbrew <iftiaqhossain@gmail.com>
2025-04-08 09:21:31 +0600

    cors has been initialized

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const cors = require("cors");
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173/"],
+    credentials:true
+}));
 app.use(cookieParser());
 
 
